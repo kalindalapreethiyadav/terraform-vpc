32d0ca17bf2eab0e0e7cba281ff0519731a464da
preethi k <kpreethiyadav579@gmail.com>
2022-09-29 23:26:09 +0530

    adding

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "preethi-basic-setup"
-    key    = "robot-basic-setup/terraform.tfstate"
+    key    = "dev/vpc/terraform.tfstate"
     region = "us-east-1"
   }
 }
