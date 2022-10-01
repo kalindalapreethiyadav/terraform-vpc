174caa2d7a7f675937521cbde1b20bbe040cc2e7
preethi k <kpreethiyadav579@gmail.com>
2022-10-01 09:09:59 +0530

    adding

--- main.tf
+++ main.tf
@@ -1,4 +1,17 @@
+
 module "vpc" {
-  source = "./vendor/modules/vpc"
-  VPC_CIDR = var.VPC_CIDR
+  source           = "./vendor/modules/vpc"
+  VPC_CIDR         = var.VPC_CIDR
+  ENV              = var.ENV
+  AZ               = var.AZ
+  DEFAULT_VPC_ID   = var.DEFAULT_VPC_ID
+  DEFAULT_VPC_CIDR = var.DEFAULT_VPC_CIDR
+  DEFAULT_VPC_RT   = var.DEFAULT_VPC_RT
+  PUBLIC_SUBNET_CIDR  = var.PUBLIC_SUBNET_CIDR
+  PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
+  HOSTEDZONE_PRIVATE_ID   =  var.HOSTEDZONE_PRIVATE_ID
+  HOSTEDZONE_PRIVATE_ZONE = var.HOSTEDZONE_PRIVATE_ZONE
+  HOSTEDZONE_PUBLIC_ID    = var.HOSTEDZONE_PUBLIC_ID 
+  HOSTEDZONE_PUBLIC_ZONE  = var.HOSTEDZONE_PUBLIC_ZONE
+
 }
